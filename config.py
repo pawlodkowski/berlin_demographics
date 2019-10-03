e8c92d24c27313d8da0693c18c66b0c1d0321d2b
Paul Wlodkowski <pawlodkowski@gmail.com>
2019-10-03 17:18:33 +0200

    moved constants to a separate script

---
+++ config.py
@@ -0,0 +1,18 @@
+CONFIG = {
+          'SOURCE_FILE' : 'data/EWRMIGRA201712H_Matrix.csv',
+          'CHROME_PATH' : 'open -a /Applications/Google\ Chrome.app %s',
+          'LOR_KEY' : 'data/LOR-Schluesselsystematik.xls',
+          'GEO_JSON' : 'data/LOR-Bezirksregionen.geojson',
+          'KEY_COLS' : ['BEZ', 'Bezirk', 'PGR', 'Prognoseraum',
+                        'BZR', 'Bezirksregion', 'PLR', 'Planungsraum'],
+          'OLD_COLS' : ['HK_EU15', 'HK_EU28', 'HK_Polen',
+                        'HK_EheJug', 'HK_EheSU', 'HK_Turk',
+                        'HK_Arab', 'HK_Sonst', 'HK_NZOrd'],
+          'NEW_COLS' : ['%EU15', '%EU28', '%Poland',
+                        '%Form. Yug.', '%Form. USSR', '%Turk',
+                        '%Arab', '%Other', '%Unclassified'],
+          'CLI_OPTIONS' : ['EU-15 Nations', 'EU-28 Nations', 'Poland',
+                           'Balkans (i.e. former Yugoslavia)',
+                           'Russia / Former USSR', 'Turkey',
+                           'Arab Nations', 'Other', 'Unclassified']
+                        }
